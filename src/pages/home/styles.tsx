85e693c94af2d80f7798148429ba273d7106d515
pze <pze@pop-os.localdomain>
2023-02-26 14:41:10 -0300

    homepage complete

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Navbar = styled.section`
-    height: 3em;
+    height: 5vh;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -12,6 +12,39 @@ export const Navbar = styled.section`
         cursor: pointer;
     }
     a:hover{
+        color: orange;
+    }
+`
+export const Container = styled.section`
+    height: 95vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const InfoBox = styled.div`
+    width: 60%;
+    /* background-color: gray; */
+    height: 70vh;
+    color: white;
+    display: flex;
+    img {
+        width: 60%;
+        padding: 0 12vh;
+    }
+`
+
+export const TextBox = styled.div`
+    width: 60%;
+    /* background-color: blue; */
+    display: flex;
+    flex-direction: column;
+    span{
+        font-size: 30px;
+    }
+
+    .h1{
+        font-size: 40px;
         color: orange;
     }
 `
