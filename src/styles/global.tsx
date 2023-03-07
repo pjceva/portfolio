ce237a75da411855be12c0a48fd38d8c3ec59419
pjceva <pjceva@gmail.com>
2023-03-07 13:03:30 -0300

    Global fonts

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -8,6 +8,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Open Sans', sans-serif;
         background-color: black;
         color: white;
         .h1{
