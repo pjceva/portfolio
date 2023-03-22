a627625b423d11a4bea35f2502a4222df94723d7
pjceva <pjceva@gmail.com>
2023-03-22 17:08:11 -0300

    First changes

--- src/pages/skills/skills.tsx
+++ src/pages/skills/skills.tsx
@@ -1,6 +1,6 @@
 import { Content } from "../../components/Content/styles"
 import { Screen } from "../../components/Screen/styles"
-import { Card } from "./styles"
+import { Card, Container } from "./styles"
 import python from "../../imgs/python.png"
 import reactjs from "../../imgs/react.png"
 import github from "../../imgs/git.png"
@@ -9,7 +9,28 @@ import rails from "../../imgs/rails.png"
 export const Skills = () => {
     return (
         <Screen id="Skill">
+            
             <span className="h1">What did I learn?</span>
+            <Container>
+                <div className="Img1">
+                    <div className="one">
+                        <h1>Image 1</h1>
+                    </div>
+
+                </div>
+                <div className="Img2">
+                <div className="two">
+                    <h1>Image 2</h1>
+                </div>
+                </div>
+                <div className="Img3">
+                    <div className="three">
+                        <h1>Image 3</h1>
+                    </div>
+                </div>
+            </Container>
+
+
             {/* <Content>
                 <Card><img src={python}/></Card>
                 <Card><img src={reactjs}/></Card>
--- src/pages/skills/styles.tsx
+++ src/pages/skills/styles.tsx
@@ -22,3 +22,20 @@ export const Card = styled.section`
     }
 
 `
+
+export const Container = styled.div`
+    max-width: 1200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
+    /* transform: translate(-50%, -50%); */
+    overflow: hidden;
+
+    .Img1{
+        height: 40%;
+        width: 30%;
+        background-image: url(python.png)
+    }
+    
+`
