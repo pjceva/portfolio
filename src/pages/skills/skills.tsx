79cf1700bc7a15cf5764cbc65af134ed868eee46
pze <pze@pop-os.localdomain>
2023-04-06 10:36:55 -0300

    removing wrong imports

--- src/pages/skills/skills.tsx
+++ src/pages/skills/skills.tsx
@@ -1,9 +1,5 @@
 import { Screen } from "../../components/Screen/styles"
 import { Container } from "./styles"
-import python from "../../imgs/python.png"
-import reactjs from "../../imgs/react.png"
-import github from "../../imgs/git.png"
-import rails from "../../imgs/rails.png"
 
 export const Skills = () => {
     return (
